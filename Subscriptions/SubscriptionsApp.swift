55de888c67ee5753e16eed19741dcff5cb8a59e7
Artemii <artemiithefrog95@icloud.com>
2023-11-10 21:05:30 +0100

    Initial Commit

---
+++ Subscriptions/SubscriptionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionsApp.swift
+//  Subscriptions
+//
+//  Created by artemiithefrog . on 10.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
