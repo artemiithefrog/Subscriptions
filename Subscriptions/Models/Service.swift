4e7aa9063d5e79f9246d285b7849541357d96192
Artemii <artemiithefrog95@icloud.com>
2023-11-13 22:14:02 +0100

    Added service model

---
+++ Subscriptions/Models/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Servise.swift
+//  Subscriptions
+//
+//  Created by artemiithefrog . on 13.11.2023.
+//
+
+import SwiftUI
+
+struct Service: Identifiable {
+    var id = UUID()
+    var icon: String
+    var color: Color
+    var name: String
+}
---
+++ Subscriptions/ViewModels/ServicesViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ServisesViewModel.swift
+//  Subscriptions
+//
+//  Created by artemiithefrog . on 13.11.2023.
+//
+
+import SwiftUI
+
+class ServicesViewModel {
+    var servises: [Service] = [Service(icon: "youtube", color: Color(hex: "F61D0D"), name: "Youtube Premium")]
+    
+}
