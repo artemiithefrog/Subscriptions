9959eee58b0d0bfc82d0e21430ea28fb33cb79a5
Artemii <artemiithefrog95@icloud.com>
2023-11-19 17:16:47 +0100

    In Subscription model added new items

--- Subscriptions/Models/Subscription.swift
+++ Subscriptions/Models/Subscription.swift
@@ -10,20 +10,41 @@ import SwiftData
 
 @Model
 class Subscription {
+    
     var name: String
+    var cost: String
     var desc: String
     var icon: String
     var color: String
-    var cost: String
+    var notes: String
+    
+    var firstBillDate: Date
+    
+    var selectedCycle: String
+    var selectedCyclePeriod: Int
+    var selectedCycleDate: String
+    
+    var selectedDay: String
+    var selectedDate: Int
+    var selectedTime: String
     
     var notificationId: String
-
-    init(name: String, desc: String, icon: String, color: String, cost: String, notificationId: String) {
+    
+    init(name: String, cost: String, desc: String, icon: String, color: String, notes: String, firstBillDate: Date, selectedCycle: String, selectedCyclePeriod: Int, selectedCycleDate: String, selectedDay: String, selectedDate: Int, selectedTime: String, notificationId: String) {
         self.name = name
+        self.cost = cost
         self.desc = desc
         self.icon = icon
         self.color = color
-        self.cost = cost
+        self.notes = notes
+        self.firstBillDate = firstBillDate
+        self.selectedCycle = selectedCycle
+        self.selectedCyclePeriod = selectedCyclePeriod
+        self.selectedCycleDate = selectedCycleDate
+        self.selectedDay = selectedDay
+        self.selectedDate = selectedDate
+        self.selectedTime = selectedTime
         self.notificationId = notificationId
     }
+    
 }
