ae5cee9d7d50dd3d08a410e7626e828872623a2d
Artemii <artemiithefrog95@icloud.com>
2023-11-20 13:58:55 +0100

    Added icons for CustomSubscription view

---
+++ Subscriptions/Views/Custom Subscription/CustomSubscriptionIcon.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSubscriptionIcon.swift
+//  Subscriptions
+//
+//  Created by artemiithefrog . on 20.11.2023.
+//
+
+import SwiftUI
+
+struct CustomSubscriptionIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomSubscriptionIcon()
+}
