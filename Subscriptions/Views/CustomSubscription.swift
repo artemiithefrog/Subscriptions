9aa0166fbeb7770b21504f21a593d7e8e32ec179
Artemii <artemiithefrog95@icloud.com>
2023-11-20 13:26:40 +0100

    Created CustomSubscription view

---
+++ Subscriptions/Views/CustomSubscription.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSubscription.swift
+//  Subscriptions
+//
+//  Created by artemiithefrog . on 20.11.2023.
+//
+
+import SwiftUI
+
+struct CustomSubscription: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomSubscription()
+}
