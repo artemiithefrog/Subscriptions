5aacd47544efd906c25f3533f27c3f01056b0a1e
Artemii <artemiithefrog95@icloud.com>
2023-11-21 14:44:51 +0100

    Added more default services

--- Subscriptions/ViewModels/ServicesViewModel.swift
+++ Subscriptions/ViewModels/ServicesViewModel.swift
@@ -9,21 +9,20 @@ import SwiftUI
 
 class ServicesViewModel {
     var servises: [Service] = [
-        Service(icon: "youtube", color: Color(hex: "F61D0D"), name: "Youtube Premium"),
-        Service(icon: "youtube", color: Color(hex: "F61D0D"), name: "Youtube Premium"),
-        Service(icon: "youtube", color: Color(hex: "F61D0D"), name: "Youtube Premium"),
-        Service(icon: "youtube", color: Color(hex: "F61D0D"), name: "Youtube Premium"),
-        Service(icon: "youtube", color: Color(hex: "F61D0D"), name: "Youtube Premium"),
-        Service(icon: "youtube", color: Color(hex: "F61D0D"), name: "Youtube Premium"),
-        Service(icon: "youtube", color: Color(hex: "F61D0D"), name: "Youtube Premium"),
-        Service(icon: "youtube", color: Color(hex: "F61D0D"), name: "Youtube Premium"),
-        Service(icon: "youtube", color: Color(hex: "F61D0D"), name: "Youtube Premium"),
-        Service(icon: "youtube", color: Color(hex: "F61D0D"), name: "Youtube Premium"),
-        Service(icon: "youtube", color: Color(hex: "F61D0D"), name: "Youtube Premium"),
-        Service(icon: "youtube", color: Color(hex: "F61D0D"), name: "Youtube Premium"),
-        Service(icon: "youtube", color: Color(hex: "F61D0D"), name: "Youtube Premium"),
-        Service(icon: "youtube", color: Color(hex: "F61D0D"), name: "Youtube Premium"),
-        Service(icon: "youtube", color: Color(hex: "F61D0D"), name: "Youtube Premium")
+        Service(icon: "apple", color: Color(hex: "000000"), name: "Apple Music"),
+        Service(icon: "creative-cloud", color: Color(hex: "D61301"), name: "Creative Cloud"),
+        Service(icon: "deezer", color: Color(hex: "000000"), name: "Deezer"),
+        Service(icon: "dribble", color: Color(hex: "E94E89"), name: "Dribble"),
+        Service(icon: "dropbox-logo", color: Color(hex: "0062FE"), name: "Dropbox"),
+        Service(icon: "evernote", color: Color(hex: "7AC142"), name: "Evernote"),
+        Service(icon: "google-drive", color: Color(hex: "21A363"), name: "Google Drive"),
+        Service(icon: "hbo", color: Color(hex: "000000"), name: "HBO"),
+        Service(icon: "icloud", color: Color(hex: "217CF0"), name: "iCloud"),
+        Service(icon: "netflix", color: Color(hex: "E40000"), name: "Netflix"),
+        Service(icon: "playstation", color: Color(hex: "00006F"), name: "Playstation Plus"),
+        Service(icon: "spotify", color: Color(hex: "2FBE58"), name: "Spotify"),
+        Service(icon: "verizon", color: Color(hex: "FE3636"), name: "Verizon"),
+        Service(icon: "youtube", color: Color(hex: "E60001"), name: "Youtube Premium")
     ]
     
 }
