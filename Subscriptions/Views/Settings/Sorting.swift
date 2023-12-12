5648753a88817a0dbef447e918555082a0bea56b
Artemii <artemiithefrog95@icloud.com>
2023-12-12 22:02:46 +0100

    Made Sorting view

--- Subscriptions/Views/Settings/Sorting.swift
+++ Subscriptions/Views/Settings/Sorting.swift
@@ -8,8 +8,53 @@
 import SwiftUI
 
 struct Sorting: View {
+    
+    let items = ["Latest", "Alphabetical (A - Z)", "Alphabetical (Z - A)", "Price (High - Low)", "Price (Low - High)"]
+    @State private var selectedItem = "Latest"
+    
+    @EnvironmentObject var settingsVM: SettingsViewModel
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                ForEach(items, id: \.self) { item in
+                    HStack {
+                        if item == selectedItem {
+                            HStack {
+                                Text(item)
+                                Spacer()
+                                Image(systemName: "checkmark")
+                            }
+                        } else {
+                            Text(item)
+                        }
+                    }
+                    .onTapGesture {
+                        selectedItem = item
+                    }
+                }
+                Text("")
+                Text("selected item: \(selectedItem)")
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        UserDefaults.standard.setValue(selectedItem, forKey: "APP_SORTING")
+                        settingsVM.selectedSorting = selectedItem
+                        dismiss()
+                    } label: {
+                        Text("Save")
+                    }
+                }
+            }
+            .onAppear {
+                selectedItem = settingsVM.selectedSorting
+            }
+            .onChange(of: settingsVM.selectedSorting) {
+                print("view model changed  \(settingsVM.selectedSorting)")
+            }
+        }
     }
 }
 
