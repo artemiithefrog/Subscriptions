7643ece5d2c8b450aea402cff3570251d89bdfc7
Artemii <artemiithefrog95@icloud.com>
2023-12-16 21:28:01 +0100

    Created UI for ViewTotalAs view

--- Subscriptions/Views/Settings/ViewTotalAs.swift
+++ Subscriptions/Views/Settings/ViewTotalAs.swift
@@ -8,9 +8,77 @@
 import SwiftUI
 
 struct ViewTotalAs: View {
+    
+    let items: [TotalModel] = [
+        TotalModel(title: "Average Expenses", description: "Divides the total of all bills into the period of your choosing."),
+        TotalModel(title: "Remaining Expenses", description: "Shows a total of the bills you have left to pay rhis period."),
+        TotalModel(title: "Total Expenses", description: "Shows a total of the bills you have in this entire period."),
+    ]
+    @State private var selectedItem = "Average Expenses"
+    
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                ForEach(items, id: \.self) { item in
+                    HStack {
+                        if item.title == selectedItem {
+                            HStack {
+                                Image(systemName: "checkmark")
+                                VStack(alignment: .leading) {
+                                    Text(item.title)
+                                    Text(item.description)
+                                        .font(.system(size: 15))
+                                        .fontWeight(.light)
+                                        .foregroundColor(.gray)
+                                }
+                            }
+                        } else {
+                            HStack {
+                                Image(systemName: "checkmark")
+                                    .opacity(0)
+                                VStack(alignment: .leading) {
+                                    Text(item.title)
+                                    Text(item.description)
+                                        .font(.system(size: 15))
+                                        .fontWeight(.light)
+                                        .foregroundColor(.gray)
+                                }
+                            }
+                        }
+                    }
+                    .onTapGesture {
+                        selectedItem = item.title
+                    }
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden()
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Image(systemName: "chevron.left")
+                    }
+                    .tint(Color("ButtonTextGreen"))
+                }
+                ToolbarItem(placement: .principal) {
+                    Text("View Total as")
+                        .bold()
+                        .font(.system(size: 20))
+                }
+            }
+        }
     }
+}
+
+struct TotalModel: Codable, Hashable {
+    
+    var title: String
+    var description: String
+    
 }
 
 #Preview {
