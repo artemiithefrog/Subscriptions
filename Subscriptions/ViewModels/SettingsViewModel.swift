8e92c6d9cb16c1b72326cb2e6620c4ec6994e359
Artemii <artemiithefrog95@icloud.com>
2023-12-18 22:59:59 +0100

    Deleted extra code

--- Subscriptions/ViewModels/SettingsViewModel.swift
+++ Subscriptions/ViewModels/SettingsViewModel.swift
@@ -11,7 +11,6 @@ import SwiftUI
 class SettingsViewModel: ObservableObject {
     
     @Published var selectedSorting = UserDefaults.standard.string(forKey: "APP_SORTING") ?? "Latest"
-    @Published var selectedTheme = UserDefaults.standard.string(forKey: "APP_THEME") ?? "System"
     @AppStorage("isDarkMode") var appTheme = UserDefaults.standard.bool(forKey: "SELECTED_THEME")
     
 }
