a8e0136bc528fe7d3d5ada39ac7f113bd860411e
Artemii <artemiithefrog95@icloud.com>
2023-12-19 18:37:01 +0100

    Created Currency model

---
+++ Subscriptions/Models/Currency.swift
@@ -0,0 +1,16 @@
+//
+//  Currency.swift
+//  Subscriptions
+//
+//  Created by artemiithefrog . on 18.12.2023.
+//
+
+import Foundation
+
+struct Currency: Codable, Hashable {
+    
+    var name: String
+    var description: String
+    var currencyCode: String
+    
+}
