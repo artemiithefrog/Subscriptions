f6c7dc39cbb26d5b62b512079fedfbe3e0c5c11c
Artemii <artemiithefrog95@icloud.com>
2023-12-20 15:06:18 +0100

    Added selectedCurrencySymbol and selectedCurrency to settings ViewModel

--- Subscriptions/ViewModels/SettingsViewModel.swift
+++ Subscriptions/ViewModels/SettingsViewModel.swift
@@ -11,6 +11,8 @@ import SwiftUI
 class SettingsViewModel: ObservableObject {
     
     @Published var selectedSorting = UserDefaults.standard.string(forKey: "APP_SORTING") ?? "Latest"
+    @Published var selectedCurrencySymbol = UserDefaults.standard.string(forKey: "SELECTED_CURRENCYSYMBOL") ?? "$"
+    @Published var selectedCurrency = UserDefaults.standard.string(forKey: "SELECTED_CURRENCY") ?? "🇺🇸 United States ($)"
     @AppStorage("APP_THEME") var appTheme = "System"
     
 }
